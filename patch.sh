d14b3b054d4e6dfbe0563cbdd41f17fe41be61e8
DarthChungo <antonio@adaro.eu>
2023-12-30 16:54:06 +0100

    Update patch script to be more verbose

--- patch.sh
+++ patch.sh
@@ -5,6 +5,8 @@ cd "$(dirname "$0")"
 rev='9846a56'
 date=$(date '+%Y%m%d')
 
+echo "==> getting sources"
+
 if [ ! -d 'st' ] || [ ! -d 'st/.git' ]; then
   rm -rfv 'st'
   git clone git://git.suckless.org/st st
@@ -18,9 +20,18 @@ else
   git checkout $rev
 fi
 
+echo "==> applying patches"
+
 for f in ../patches/*.diff; do
-  patch -p1 --no-backup-if-mismatch < $f
+  if ! patch -p1 --no-backup-if-mismatch < $f; then
+    echo "=> failed to apply patch $(basename $f), aborting..."
+    exit
+  else
+    echo "=> correctly applied patch $(basename $f)"
+  fi
 done
+
+echo "==> generaing unified patch file"
 
 git add .
 git commit -m 'st-patched: apply patches'
